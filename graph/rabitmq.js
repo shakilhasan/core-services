fb66a9e3796bb440fb48ce8f4ccb8cb006792b84
Md.Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-04-20 13:23:52 +0600

    add basic scaffolding

---
+++ graph/rabitmq.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+// https://github.com/rabbitmq/rabbitmq-tutorials/blob/master/javascript-nodejs/src/emit_log.js
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var exchange = 'logs';
+        var msg = process.argv.slice(2).join(' ') || 'Hello World!';
+
+        channel.assertExchange(exchange, 'fanout', {
+            durable: false
+        });
+        channel.publish(exchange, '', Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+    });
+
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
+
+#!/usr/bin/env node
+// https://github.com/rabbitmq/rabbitmq-tutorials/blob/master/javascript-nodejs/src/receive_logs.js
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var exchange = 'logs';
+
+        channel.assertExchange(exchange, 'fanout', {
+            durable: false
+        });
+
+        channel.assertQueue('', {
+            exclusive: true
+        }, function(error2, q) {
+            if (error2) {
+                throw error2;
+            }
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+            channel.bindQueue(q.queue, exchange, '');
+
+            channel.consume(q.queue, function(msg) {
+                if(msg.content) {
+                    console.log(" [x] %s", msg.content.toString());
+                }
+            }, {
+                noAck: true
+            });
+        });
+    });
+});
